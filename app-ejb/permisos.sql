56c06d50f8141dcbd3daf9128d04cfc1b886a08a
lemaedu <lemaedu@gmail.com>
2020-01-04 15:00:22 -0500

    update schema

--- app-ejb/permisos.sql
+++ app-ejb/permisos.sql
@@ -1,11 +1,11 @@
-CREATE TABLE "tb_acceso" (
-"id_acceso" serial4 NOT NULL,
-"_id_opcion" int4,
-"_id_rol" int4,
-"status" bool,
-"user_create" int8,
-"user_update" int8,
-"user_delete" int8,
+REATE TABLE "tb_acceso" (
+"id_acceso" serial2 NOT NULL,
+"_id_opcion" int2,
+"_id_rol" int2,
+"status" bool DEFAULT true,
+"user_create" integer,
+"user_update" integer,
+"user_delete" integer,
 "date_create" date,
 "date_update" date,
 "date_delete" date,
@@ -25,14 +25,14 @@ COMMENT ON COLUMN "tb_acceso"."date_delete" IS 'fecha baja';
 ALTER TABLE "tb_acceso" OWNER TO "postgres";
 
 CREATE TABLE "tb_menu" (
-"id_menu" serial4 NOT NULL,
+"id_menu" serial2 NOT NULL,
 "nombre" varchar(50),
 "img" varchar(75),
-"_id_ubicacion_menu" int4,
+"_id_ubicacion_menu" int2,
 "status" bool DEFAULT true,
-"user_create" int8,
-"user_update" int8,
-"user_delete" int8,
+"user_create" integer,
+"user_update" integer,
+"user_delete" integer,
 "date_create" date,
 "date_update" date,
 "date_delete" date,
@@ -53,16 +53,16 @@ COMMENT ON COLUMN "tb_menu"."date_delete" IS 'fecha de baja';
 ALTER TABLE "tb_menu" OWNER TO "postgres";
 
 CREATE TABLE "tb_opcion" (
-"id_opcion" serial4 NOT NULL,
-"_id_menu" int4,
+"id_opcion" serial2 NOT NULL,
+"_id_menu" int2,
 "nombre" varchar(50),
 "link" varchar(150),
 "img" varchar(100),
-"orden" int4,
-"status" bool,
-"user_create" int8,
-"user_update" int8,
-"user_delete" int8,
+"orden" int2,
+"status" bool DEFAULT true,
+"user_create" integer,
+"user_update" integer,
+"user_delete" integer,
 "date_create" date,
 "date_update" date,
 "date_delete" date,
@@ -85,14 +85,14 @@ COMMENT ON COLUMN "tb_opcion"."date_delete" IS 'fecha de baja';
 ALTER TABLE "tb_opcion" OWNER TO "postgres";
 
 CREATE TABLE "tb_rol" (
-"id_rol" serial4 NOT NULL,
+"id_rol" serial2 NOT NULL,
 "nombre" varchar(50),
 "descripcion" varchar(100),
-"_id_modulo" int4,
-"status" bool,
-"user_create" int8,
-"user_update" int8,
-"user_delete" int8,
+"_id_modulo" int2,
+"status" bool DEFAULT true,
+"user_create" integer,
+"user_update" integer,
+"user_delete" integer,
 "date_create" date,
 "date_update" date,
 "date_delete" date,
@@ -102,7 +102,7 @@ WITHOUT OIDS;
 COMMENT ON COLUMN "tb_rol"."nombre" IS 'guarda nombre rol';
 COMMENT ON COLUMN "tb_rol"."descripcion" IS 'guarda descripcio de rol ';
 COMMENT ON COLUMN "tb_rol"."_id_modulo" IS 'fk (pk tb_modulo)';
-COMMENT ON COLUMN "tb_rol"."status" IS 'Estado, (true=activo, false=inactivo)';
+COMMENT ON COLUMN "tb_rol"."status" IS 'estado (true=activo,false=inactivo)';
 COMMENT ON COLUMN "tb_rol"."user_create" IS 'usuario alta';
 COMMENT ON COLUMN "tb_rol"."user_update" IS 'usuario ultima modifica';
 COMMENT ON COLUMN "tb_rol"."user_delete" IS 'Usuario baja';
@@ -112,12 +112,12 @@ COMMENT ON COLUMN "tb_rol"."date_delete" IS 'fecha baja';
 ALTER TABLE "tb_rol" OWNER TO "postgres";
 
 CREATE TABLE "tb_ubicacion_menu" (
-"id_ubicacion_menu" serial4 NOT NULL,
+"id_ubicacion_menu" serial2 NOT NULL,
 "ubicacion" varchar(50),
 "status" bool DEFAULT true,
-"user_create" int8,
-"user_update" int8,
-"user_delete" int8,
+"user_create" integer,
+"user_update" integer,
+"user_delete" integer,
 "date_create" timestamp,
 "date_update" timestamp,
 "date_delete" timestamp,
@@ -126,7 +126,7 @@ CONSTRAINT "tb_ubicacion_menu_pkey" PRIMARY KEY ("id_ubicacion_menu")
 WITHOUT OIDS;
 COMMENT ON COLUMN "tb_ubicacion_menu"."id_ubicacion_menu" IS 'clave principal';
 COMMENT ON COLUMN "tb_ubicacion_menu"."ubicacion" IS 'Ubicacion, (principal, superios, inferios, izq, derecho)';
-COMMENT ON COLUMN "tb_ubicacion_menu"."status" IS 'estado(true=vigente, false=no vigente)';
+COMMENT ON COLUMN "tb_ubicacion_menu"."status" IS 'estado (true=activo,false=inactivo)';
 COMMENT ON COLUMN "tb_ubicacion_menu"."user_create" IS 'usuario que da alta';
 COMMENT ON COLUMN "tb_ubicacion_menu"."user_update" IS 'usuario de ultima modificacion';
 COMMENT ON COLUMN "tb_ubicacion_menu"."user_delete" IS 'usuario q da baja';
@@ -136,17 +136,17 @@ COMMENT ON COLUMN "tb_ubicacion_menu"."date_delete" IS 'fecha de baja';
 ALTER TABLE "tb_ubicacion_menu" OWNER TO "postgres";
 
 CREATE TABLE "tb_usuario" (
-"id_user" serial8 NOT NULL,
-"_id_persona" int8,
+"id_user" serial4 NOT NULL,
+"_id_persona" integer,
 "alias" varchar(200),
 "correo" varchar(50),
 "clave" varchar(100),
 "clave_tem" varchar(100),
 "link" varchar(150),
-"status" bool,
-"user_create" int8,
-"user_update" int8,
-"user_delete" int8,
+"status" bool DEFAULT true,
+"user_create" integer,
+"user_update" integer,
+"user_delete" integer,
 "date_create" date,
 "date_update" date,
 "date_delete" date,
@@ -160,7 +160,7 @@ COMMENT ON COLUMN "tb_usuario"."alias" IS 'campo usuado para autenticar';
 COMMENT ON COLUMN "tb_usuario"."clave" IS 'guarda clave cifrada';
 COMMENT ON COLUMN "tb_usuario"."clave_tem" IS 'clave temporal';
 COMMENT ON COLUMN "tb_usuario"."link" IS 'guarda link de acceso';
-COMMENT ON COLUMN "tb_usuario"."status" IS '0=desactivado, 1=activado';
+COMMENT ON COLUMN "tb_usuario"."status" IS 'estado (true=activo,false=inactivo)';
 COMMENT ON COLUMN "tb_usuario"."user_create" IS 'guarda el usuario que registra';
 COMMENT ON COLUMN "tb_usuario"."user_update" IS 'guarda usuario q actualiza';
 COMMENT ON COLUMN "tb_usuario"."user_delete" IS 'Usuario que da baja';
@@ -170,20 +170,20 @@ COMMENT ON COLUMN "tb_usuario"."date_delete" IS 'fecha de baja';
 ALTER TABLE "tb_usuario" OWNER TO "postgres";
 
 CREATE TABLE "tb_usuario_rol" (
-"id_usuario_rol" serial4 NOT NULL,
-"_id_rol" int4,
-"_id_usuario" int8,
+"id_usuario_rol" serial2 NOT NULL,
+"_id_rol" int2,
+"_id_user" integer,
 "status" bool DEFAULT true,
-"user_create" int8,
-"user_update" int8,
-"user_delete" int8,
+"user_create" integer,
+"user_update" integer,
+"user_delete" integer,
 "date_create" date,
 "date_update" date,
 "date_delete" date,
 CONSTRAINT "tb_usuario_rol_pkey" PRIMARY KEY ("id_usuario_rol") 
 )
 WITHOUT OIDS;
-COMMENT ON COLUMN "tb_usuario_rol"."status" IS 'estado';
+COMMENT ON COLUMN "tb_usuario_rol"."status" IS 'estado (true=activo,false=inactivo)';
 COMMENT ON COLUMN "tb_usuario_rol"."user_create" IS 'usuario q da alta';
 COMMENT ON COLUMN "tb_usuario_rol"."user_update" IS 'usuario ultima actualizacion';
 COMMENT ON COLUMN "tb_usuario_rol"."user_delete" IS 'usuario que da baja';
@@ -193,13 +193,13 @@ COMMENT ON COLUMN "tb_usuario_rol"."date_delete" IS 'fecha de baja';
 ALTER TABLE "tb_usuario_rol" OWNER TO "postgres";
 
 CREATE TABLE "tb_modulo" (
-"id_modulo" serial4 NOT NULL,
-"nombre" text,
-"descripcion" text,
-"status" bool,
-"user_create" int8,
-"user_update" int8,
-"user_delete" int8,
+"id_modulo" serial2 NOT NULL,
+"nombre" varchar(100),
+"descripcion" varchar(100),
+"status" bool DEFAULT true,
+"user_create" integer,
+"user_update" integer,
+"user_delete" integer,
 "date_create" date,
 "date_update" date,
 "date_delete" date,
@@ -208,7 +208,7 @@ PRIMARY KEY ("id_modulo")
 WITHOUT OIDS;
 COMMENT ON COLUMN "tb_modulo"."id_modulo" IS 'pk tabla';
 COMMENT ON COLUMN "tb_modulo"."nombre" IS 'nombre del modulo(financiero,reportes)';
-COMMENT ON COLUMN "tb_modulo"."status" IS 'estado (true=activo, false=inactivo)';
+COMMENT ON COLUMN "tb_modulo"."status" IS 'estado (true=activo,false=inactivo)';
 COMMENT ON COLUMN "tb_modulo"."user_create" IS 'Usuario Alta';
 COMMENT ON COLUMN "tb_modulo"."user_update" IS 'Usuario ultima actualizacion';
 COMMENT ON COLUMN "tb_modulo"."user_delete" IS 'usuario baja';
